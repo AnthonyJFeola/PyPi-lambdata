88dd25891a375f393dda89bcf1e5ea8f9d2ff6c7
Anthony Feola <anthonyfeola@Anthonys-MBP.fios-router.home>
2020-05-10 20:07:13 -0400

    Package complete. Updated README.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="lambdata-AnthonyJFeola-V2", # the name that you will install via pip
+    name="lambdata-AnthonyJFeolaV2", # the name that you will install via pip
     version="2.0",
     author="Anthony J Feola",
     author_email="anthonyfeola18@gmail.com",

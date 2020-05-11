914067b8fe594cd00e87daa3ecf988a95dbd29c3
Anthony Feola <anthonyfeola@Anthonys-MBP.fios-router.home>
2020-05-10 20:02:14 -0400

    Package complete. Updated README.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-AnthonyJFeola", # the name that you will install via pip
-    version="1.0",
+    version="2.0",
     author="Anthony J Feola",
     author_email="anthonyfeola18@gmail.com",
     description="Hyperlink removal tool",
